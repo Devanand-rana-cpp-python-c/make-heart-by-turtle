e823a07a6c42a59acfa7d97c63dc808f6a62aa8c
Devanand-rana-cpp-python-c <98279945+Devanand-rana-cpp-python-c@users.noreply.github.com>
2022-01-29 14:19:49 +0530

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,31 @@
+import turtle
+pen= turtle.Turtle()
+def curve():
+    for i in range (200):
+        pen.right(1)
+        pen.forward(1)
+
+def heart():
+    pen.fillcolor("black")
+    pen.begin_fill()
+    
+    pen.left(140)
+    pen.forward(113)
+    
+    curve()
+    pen.left(120)
+    
+    curve()
+    
+    pen.forward(112)
+    pen.end_fill()
+
+def text():
+    pen.up()
+    pen.setpos(-55,50)
+    pen.down()
+    pen.color("red")
+    pen.write("Hello heart ! ",font=("algerian",20,"bold"))
+    
+heart()
+text()
